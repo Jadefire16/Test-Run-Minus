98382ecf0bd7f9be221c48f450df59fd0fc04b9a
Zach <53870574+Jadefire16@users.noreply.github.com>
2024-01-31 02:28:30 -0500

    Added Test Cache
    Added a test cache object which will handle caching tests in the future.
    Currently only caches type names until the test runner structure is solidified.

---
+++ Core/Test Runner/ICache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Run_Minus.Core.Test_Runner
+{
+    internal interface ICache<T> : ICollection<T>, IEnumerable<T>, IComparable<T>, IComparable { }
+}
---
+++ Core/Test Runner/TestTypeCache.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Run_Minus.Core.Test_Runner
+{
+    internal class TestTypeCache : ICache<string>
+    {
+        private readonly HashSet<string> cache;
+        private readonly bool isReadonly;
+        public int Count => cache.Count;
+
+        public bool IsReadOnly => true;
+
+        public TestTypeCache(int capacity = 3)
+        {
+            cache = new HashSet<string>(capacity);
+        }
+
+        public TestTypeCache(string[] types)
+        {
+            cache = new HashSet<string>(types);
+        }
+
+        public void Add(string item) => cache.Add(item);
+
+        public void Clear() => cache.Clear();
+
+        public int CompareTo(string? other)
+        {
+            if (other == null)
+                return 1;
+            return CompareTo(other);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) 
+                return 1;
+            return obj is TestTypeCache casted ? this.CompareTo(casted) : throw new ArgumentException($"Object is not a {nameof(TestTypeCache)}");
+        }
+
+        public bool Contains(string item) => cache.Contains(item);
+
+        public void CopyTo(string[] array, int arrayIndex) => cache.CopyTo(array, arrayIndex);
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return cache.GetEnumerator();
+        }
+
+        public bool Remove(string item) => cache.Remove(item);
+        public bool RemoveAllInstances(string item)
+        {
+            if(!cache.Contains(item))
+                return false;
+            while (cache.Contains(item))
+            {
+                cache.Remove(item);
+            }
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
