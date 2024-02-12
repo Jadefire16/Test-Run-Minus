871c8e8234d8d52fe059a55fb94ed5c68b80ab2b
Zach <53870574+Jadefire16@users.noreply.github.com>
2024-02-12 01:51:09 -0500

    Refactoring and Logic Blocking
    Refactored the cache to be fully generic.
    
    Started blocking out the logic for the Test Runners, the code is garbage but should work for now. Need to implement a way to handle test parameters next.

---
+++ Core/Test Runner/Cache.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Run_Minus.Core.Test_Runner
+{
+    internal class Cache<T> : ICache<T>
+    {
+        private readonly HashSet<T> typeCache;
+        private readonly bool isReadonly;
+        public int Count => typeCache.Count;
+
+        public bool IsReadOnly => true;
+
+        public Cache(int capacity = 3)
+        {
+            typeCache = new HashSet<T>(capacity);
+        }
+
+        public Cache(IEnumerable<T> types)
+        {
+            typeCache = new HashSet<T>(types);
+        }
+
+        public void Add(T item) => typeCache.Add(item);
+
+        public void Clear() => typeCache.Clear();
+
+        public int CompareTo(T? other)
+        {
+            if (other == null)
+                return 1;
+            return CompareTo(other);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) 
+                return 1;
+            return obj is not Cache<T> casted ? throw new ArgumentException($"Object is not a {nameof(Cache<T>)}") : CompareTo(casted);
+        }
+
+        public bool Contains(T item) => typeCache.Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex) => typeCache.CopyTo(array, arrayIndex);
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return typeCache.GetEnumerator();
+        }
+
+        public bool Remove(T item) => typeCache.Remove(item);
+        public bool RemoveAllInstances(T item)
+        {
+            if(!typeCache.Contains(item))
+                return false;
+            while (typeCache.Contains(item))
+            {
+                typeCache.Remove(item);
+            }
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Core/Test Runner/TestRunner.cs
+++ Core/Test Runner/TestRunner.cs
@@ -1,26 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Test_Run_Minus.Core.Test_Runner.Attributes;
+
 namespace Test_Run_Minus.Core.Test_Runner
 {
     internal class TestRunner : ITestRunner
     {
-        private readonly HashSet<Type> types;
-        private readonly TestTypeCache cache;
-
-        public TestRunner(HashSet<string> types)
+        private Cache<Delegate> testMethods;
+        public TestRunner()
         {
-            this.types = new HashSet<Type>();
-            cache = new TestTypeCache();
+            testMethods = new Cache<Delegate>();
         }
 
         public void Initialize()
         {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            var types = asm.GetTypes().Where(t => !t.IsAbstract && t.IsClass && t.GetCustomAttribute<TestClassAttribute>() is not null);
+            IEnumerator<Type> enumerator = types.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var type = enumerator.Current;
+                if (type is null)
+                    continue;
+                var methodCache = type.GetMethods().Where(m =>
+                m.GetCustomAttribute<TestMethodAttribute>() is not null &&
+                m.GetCustomAttribute<AsyncStateMachineAttribute>() is null &&
+                !m.IsStatic &&
+                !m.IsVirtual
+                );
 
+                object? instance = Activator.CreateInstance(type);
+                if (instance is null)
+                    continue;
+                foreach (var method in methodCache)
+                {
+                    var del = Delegate.CreateDelegate(type, instance, method);
+                    if (del is null)
+                        continue;
+                    testMethods.Add(del);
+                }
+                // This is all awful for so many reasons
+            }
         }
 
         public void Run()
         {
-            
+            foreach (var method in testMethods)
+            {
+                try
+                {
+                    method.DynamicInvoke();
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.ToString());
+                }
+            }
         }
     }
 }
